6a2d0e4790d8014254d6fab7dfed33845d0d5821
Hack Reactor Students <students@hackreactor.com>
2013-11-14 10:20:48 -0800

    Copied code from prototypal stack to pseudoclassical stack, passes some tests

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,23 @@
 var Stack = function() {
   // Hey! Copy your code from src/prototypal/stack.js and paste it here
+
+  // Use an object with numeric keys to store values
+  var storage = {};
+  var length = 0; // Hint: set an initial value here
+
+  this.push = function(value){
+    this[length] = value;
+    length++;
+  },
+
+  this.pop = function(){
+    if (length) length--;
+    return this[length];
+  },
+
+  this.size = function(){
+    return length;
+  }
+
 };
+
